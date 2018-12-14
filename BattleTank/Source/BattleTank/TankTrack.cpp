2f942dda6aaf6cf4c5a15c8e676865f21dc902f2
Bmeist <brian@tigernet.com>
2018-12-14 10:49:01 -0500

    dfg

--- BattleTank/Source/BattleTank/TankTrack.cpp
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -1,9 +1,16 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankTrack.h"
+#include "GameFramework/Actor.h"
+#include "Components/PrimitiveComponent.h"
 
 void UTankTrack::SetThrottle(float Throttle)
 {
 	auto Name = GetName();
 	UE_LOG(LogTemp, Warning, TEXT("%s throttle set to %f"), *Name, Throttle);
-}
+
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+};
