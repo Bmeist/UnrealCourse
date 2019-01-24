6a87848cc87564da4b8615d09e0feee08dd72652
Bmeist <Bmeist@users.noreply.github.com>
2019-01-24 16:33:53 -0500

    asdf

--- TestingGrounds/Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ TestingGrounds/Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -2,12 +2,28 @@
 
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "AIModule/Classes/AIController.h"
+#include "PatrollingGuard.h"  //TODO remove coupling
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	// Get Patrol Points
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+
+	// Set next waypoint
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
-	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
-	UE_LOG(LogTemp, Warning, TEXT("Waypoint Index %i"), Index);
+	int32 Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	//UE_LOG(LogTemp, Warning, TEXT("Waypoint Index %i"), Index);
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	//TODO protect against empty patrol routes
+
+	// Cycle Index
+	int32 NextIndex = ((Index + 1) % PatrolPoints.Num());
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
 
 	return EBTNodeResult::Succeeded;
 }
